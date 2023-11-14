40f8bf02acbc7fefe3896578f1537f10f71efb69
Tanya <tanya777fan@mail.ru>
2023-11-14 17:51:56 +0300

    form and styles added

--- auth-form/src/App.js
+++ auth-form/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <form>      
+        <h1>Login Here</h1>
+        <label> E-mail <br></br><input type="email" name="email" placeholder="example@mail.com"/> </label>
+        <label> Password <br></br><input type="password" name="password" placeholder="minimum 6 symbols"/> </label>
+        <button>Log In</button>
+      </form>
     </div>
   );
 }
